6860e8f0c421c9dbd1e2580e1a947c2baa2595d0
szymongrabski <grabskiszymonom@gmail.com>
2024-04-20 17:08:30 +0200

    cleanup

--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/header/Header.jsx';
-import ReviewsList from '../components/productDetails/ReviewsList.jsx';
 import ReviewForm from '../components/productDetails/ReviewsForm.jsx';
 import ProductInfo from '../components/productDetails/ProductInfo.jsx';
 import ProductProvider from '../contexts/ProductContext.js';
